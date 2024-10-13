83765897ff76d09775d4c8cbde07fd5b57ca7e80
Ayaan Waqar <72092367+AyaanWaqar@users.noreply.github.com>
2024-10-13 15:04:34 +0500

    Add files via upload

---
+++ Temperature_MLXsensor/Temperature.ino
@@ -0,0 +1,45 @@
+/***************************************************
+  This is a library example for the MLX90614 Temp Sensor
+
+  Designed specifically to work with the MLX90614 sensors in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1747 3V version
+  ----> https://www.adafruit.com/products/1748 5V version
+
+  These sensors use I2C to communicate, 2 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("Adafruit MLX90614 test");
+
+  if (!mlx.begin()) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
+
+  Serial.print("Emissivity = "); Serial.println(mlx.readEmissivity());
+  Serial.println("================================================");
+  pinMode(12,HIGH);
+}
+
+void loop() {
+  Serial.print("Temperature = "); Serial.print(mlx.readObjectTempC()); Serial.println("°C");
+  Serial.print("Temperature = "); Serial.print(mlx.readObjectTempF()); Serial.println("°F");
+  
+  Serial.println();
+  delay(500);
+}
